30a1ee68e9a55127a4b7f88ff00b5375ecb98725
lodekeeper <lodekeeper@users.noreply.github.com>
2026-03-30 21:29:31 +0000

    feat: add PTC window state field and epoch processing (alpha.4)
    Implements consensus-specs v1.7.0-alpha.4 PTC window changes:
    
    - Add ptcWindow field to Gloas BeaconState (Vector[Vector[ValidatorIndex, PTC_SIZE], (2+MIN_SEED_LOOKAHEAD)*SLOTS_PER_EPOCH])
    - Add computePtc, computeBalanceWeightedSelection, computeBalanceWeightedAcceptance helpers
    - Add processPtcWindow epoch processing (runs after processProposerLookahead)
    - Add initializePtcWindow for fork transition and genesis
    - Update epochCache.getPayloadTimelinessCommittee to read from state.ptcWindow (read-through)
    - Remove independently-computed PTC arrays from epochCache
    - Update compute_balance_weighted_acceptance signature (alpha.4 perf optimization)
    
    Spec refs: consensus-specs PRs #4979, #5044

--- packages/types/src/gloas/sszTypes.ts
+++ packages/types/src/gloas/sszTypes.ts
@@ -1,9 +1,17 @@
-import {BitVectorType, ContainerType, ListBasicType, ListCompositeType, VectorCompositeType} from "@chainsafe/ssz";
+import {
+  BitVectorType,
+  ContainerType,
+  ListBasicType,
+  ListCompositeType,
+  VectorBasicType,
+  VectorCompositeType,
+} from "@chainsafe/ssz";
 import {
   BUILDER_PENDING_WITHDRAWALS_LIMIT,
   BUILDER_REGISTRY_LIMIT,
   HISTORICAL_ROOTS_LIMIT,
   MAX_PAYLOAD_ATTESTATIONS,
+  MIN_SEED_LOOKAHEAD,
   NUMBER_OF_COLUMNS,
   PTC_SIZE,
   SLOTS_PER_EPOCH,
@@ -174,6 +182,9 @@ export const ExecutionPayloadEnvelopesByRangeRequest = new ContainerType(
   {typeName: "ExecutionPayloadEnvelopesByRangeRequest", jsonCase: "eth2"}
 );
 
+export const Ptc = new VectorBasicType(ValidatorIndex, PTC_SIZE);
+export const PtcWindow = new VectorCompositeType(Ptc, (2 + MIN_SEED_LOOKAHEAD) * SLOTS_PER_EPOCH);
+
 export const BeaconBlockBody = new ContainerType(
   {
     randaoReveal: phase0Ssz.BeaconBlockBody.fields.randaoReveal,
@@ -271,6 +282,7 @@ export const BeaconState = new ContainerType(
     builderPendingWithdrawals: new ListCompositeType(BuilderPendingWithdrawal, BUILDER_PENDING_WITHDRAWALS_LIMIT), // New in GLOAS:EIP7732
     latestBlockHash: Bytes32, // New in GLOAS:EIP7732
     payloadExpectedWithdrawals: capellaSsz.Withdrawals, // New in GLOAS:EIP7732
+    ptcWindow: PtcWindow, // New in GLOAS:EIP7732
   },
   {typeName: "BeaconState", jsonCase: "eth2"}
 );
